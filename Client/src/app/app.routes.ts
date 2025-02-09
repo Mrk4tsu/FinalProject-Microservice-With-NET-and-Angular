b640406fc0dac5e919c2931510a6fb087d48d63b
MrKatsu <mrk4tsu@gmail.com>
2025-02-09 23:33:59 +0700

    rebuild project

--- Client/src/app/app.routes.ts
+++ Client/src/app/app.routes.ts
@@ -1,3 +1,14 @@
-import { Routes } from '@angular/router';
+import {Routes} from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '', loadChildren: () => import('./public/public.module').then(m => m.PublicModule)
+  },
+  {
+    path: '', loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule)
+  },
+  {
+    path: 'admin', loadChildren: () => import('./admin/admin.module').then(m => m.AdminModule)
+  },
+  { path: '', redirectTo: '/', pathMatch: 'full' }
+];
---
+++ Client/src/app/public/pages/home/home.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  imports: [],
+  templateUrl: './home.component.html',
+  styleUrls: [
+    './home.component.css',
+    '../../layout/public/public.component.css'
+  ]
+})
+export class HomeComponent {
+
+}
