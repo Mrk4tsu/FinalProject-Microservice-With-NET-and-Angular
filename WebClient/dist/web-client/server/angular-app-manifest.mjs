b4ef1c43ac924995279c342cbbc6bbc38f50cbfd
MrKatsu <mrk4tsu@gmail.com>
2025-02-15 08:01:11 +0700

    update angular setting

--- WebClient/dist/web-client/server/angular-app-manifest.mjs
+++ WebClient/dist/web-client/server/angular-app-manifest.mjs
@@ -1,7 +1,7 @@
 
 export default {
   bootstrap: () => import('./main.server.mjs').then(m => m.default),
-  inlineCriticalCss: true,
+  inlineCriticalCss: false,
   baseHref: '/',
   locale: undefined,
   routes: [
@@ -48,8 +48,7 @@ export default {
 ],
   entryPointToBrowserMapping: undefined,
   assets: {
-    'index.csr.html': {size: 6777, hash: '4ec8c5ae307907af8fa00e7e617ecb8fe544952fffdd74f779df062d5d28b228', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 1498, hash: '83388687bcd3dec5f1ff750dc9575eb98e24e0b1251d461d1484e7b9b0e7f12b', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
-    'styles-MRQYNWVT.css': {size: 260918, hash: '/cvqyT/eGbU', text: () => import('./assets-chunks/styles-MRQYNWVT_css.mjs').then(m => m.default)}
+    'index.csr.html': {size: 958, hash: 'd20e5944f3b7fc1777af02967c5c8fecef28342ce46cb9b5a40e4629ca321c21', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 1498, hash: '83388687bcd3dec5f1ff750dc9575eb98e24e0b1251d461d1484e7b9b0e7f12b', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)}
   },
 };
