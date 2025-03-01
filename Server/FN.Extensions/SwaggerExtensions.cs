41d057d21f342a8eacba4b355f5426fc459083ce
MrKatsu <mrk4tsu@gmail.com>
2025-03-02 00:59:18 +0700

    Tích hợp AI hỗ trợ tạo nội dung sản phẩm

--- Server/FN.Extensions/SwaggerExtensions.cs
+++ Server/FN.Extensions/SwaggerExtensions.cs
@@ -47,7 +47,6 @@ namespace FN.Extensions
         public static IApplicationBuilder ConfigureSwaggerExplorer(this IApplicationBuilder app)
         {
             var ev = app.ApplicationServices.GetRequiredService<IHostEnvironment>();
-            Console.WriteLine(ev.EnvironmentName);
             if (ev.IsDevelopment())
             {
                 app.UseSwagger();
